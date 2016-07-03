4d87720114782d233ebf2521d3f6699ad822842a
Sam Enoka <slwenoka@gmail.com>
2016-07-03 19:08:52 +1000

    Set up to deploy to Heroku

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,9 @@
 import Botkit from 'botkit'
 import redisStorage from 'botkit-storage-redis'
 
-const redis = redisStorage()
+const redis = redisStorage({
+  url: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+})
 
 export default Botkit.slackbot({
   storage: redis,
